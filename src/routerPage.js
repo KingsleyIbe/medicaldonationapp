f10f6c6665efcbbd1142f1f075d5a9e37d65b194
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-01 04:58:23 +0100

    Fix broken deploy app in github page

--- src/routerPage.js
+++ src/routerPage.js
@@ -9,19 +9,19 @@ import DonorPage from './pages/Donor/DonorPage';
 import LandingPage from './pages/landingPage/landingPage';
 import PatientPage from './pages/Patient/PatientPage';
 
-export default function RouterPage() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/dashboard" element={<Dashboard />} />
-        <Route exact path="/donor" element={<DonorPage />} />
-        <Route exact path="/patient" element={<PatientPage />} />
-        <Route exact path="/resetpassword" element={<ResetPassword />} />
-        <Route exact path="/forgotpassword" element={<ForgotPassword />} />
-      </Routes>
-    </>
-  );
-}
+const RouterPage = () => (
+  <>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/dashboard" element={<Dashboard />} />
+      <Route exact path="/donor" element={<DonorPage />} />
+      <Route exact path="/patient" element={<PatientPage />} />
+      <Route exact path="/resetpassword" element={<ResetPassword />} />
+      <Route exact path="/forgotpassword" element={<ForgotPassword />} />
+    </Routes>
+  </>
+);
+
+export default RouterPage;
