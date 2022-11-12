2188dc25762de038861a0dc05d670fe662bf433d
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-12 17:16:49 +0100

    Fix linters error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
-    theme: {
+  theme: {
     extend: {
       backgroundImage: {
-        'hero': "url('images/authpics.jpg')",
+        hero: "url('images/authpics.jpg')",
       },
     },
   },
   plugins: [],
-}
+};
