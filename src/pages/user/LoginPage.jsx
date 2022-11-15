e0a1e63324e23d62fc469184935b5c2108c38ec5
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-15 18:51:40 +0100

    Remove unnecessary file

--- src/pages/user/LoginPage.jsx
+++ src/pages/user/LoginPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginButton from '../../components/user/LoginButton';
 import LogoutButton from '../../components/user/LogoutButton';
+import Profile from '../../components/user/Profile';
 
 const LoginPage = () => (
   <div>
     <LoginButton />
     <LogoutButton />
+    <Profile />
   </div>
 );
 
