9f55205f3ef06abf7d3b9e4976e8293ba89682ce
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-15 21:34:12 +0100

    Fix bugs with auth0 login

---
+++ src/components/user/Kyc/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../common/Header';
+import KYC from './KYC';
+
+const UserKYC = () => (
+  <div>
+    <Header />
+    <KYC />
+  </div>
+);
+
+export default UserKYC;
