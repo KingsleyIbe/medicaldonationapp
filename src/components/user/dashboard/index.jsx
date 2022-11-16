11eac5b03d4e9881f6cf6e1e0fb82b1f9bcb1de1
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-16 15:34:00 +0100

    Add dashboard

---
+++ src/components/user/dashboard/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardHeader from './DashboardHeader';
+import SideBar from './SideBar';
+
+const UserDashBoard = () => (
+  <div>
+    <SideBar />
+    <DashboardHeader />
+  </div>
+);
+
+export default UserDashBoard;
--- src/routerPage.js
+++ src/routerPage.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/landingPage/landingPage';
+import DashboardPage from './pages/user/DashboardPage';
 import KYCPage from './pages/user/KYCPage';
-import ProfilePage from './pages/user/ProfilePage';
+// import ProfilePage from './pages/user/ProfilePage';
 
 const RouterPage = () => (
   <>
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route exact path="/kyc" element={<KYCPage />} />
-      <Route exact path="/dashboard" element={<ProfilePage />} />
+      <Route exact path="/dashboard" element={<DashboardPage />} />
       <Route exact path="*" element={<h1>PAGE NOT FOUND</h1>} />
     </Routes>
   </>
