1b7184acba63949c47c26bc13260aa536643c86b
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-16 14:07:34 +0100

    Add user profile details

--- src/components/user/Kyc/index.jsx
+++ src/components/user/Kyc/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import Header from '../../common/Header';
-// import KYC from './KYC';
-// import Title from './Title';
 import Form from './Form';
 
 const UserKYC = () => (
   <div>
     <Header />
-    {/* <KYC /> */}
     <div className="flex flex-col justify-center items-center px-5 lg:px-0">
-      {/* <Title /> */}
       <Form />
     </div>
   </div>
