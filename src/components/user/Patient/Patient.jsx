4672f0c6116508c78906593e4999a9800cb5dd07
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-16 13:21:57 +0100

    Add dashboard

---
+++ src/components/user/Patient/Patient.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Patient = () => (
+  <div>Patient Page</div>
+);
+
+export default Patient;
--- src/routerPage.js
+++ src/routerPage.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-// import ForgotPassword from './pages/auth/forgotPassword';
-// import Login from './pages/auth/login';
-// import Register from './pages/auth/register';
-// import ResetPassword from './pages/auth/resetPassword';
-// import Dashboard from './pages/dashboard/Dashboard';
-// import DonorPage from './pages/Donor/DonorPage';
 import LandingPage from './pages/landingPage/landingPage';
 import KYCPage from './pages/user/KYCPage';
-// import PatientPage from './pages/Patient/PatientPage';
+import ProfilePage from './pages/user/ProfilePage';
 
 const RouterPage = () => (
   <>
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route exact path="/kyc" element={<KYCPage />} />
+      <Route exact path="/dashboard" element={<ProfilePage />} />
       <Route exact path="*" element={<h1>PAGE NOT FOUND</h1>} />
-      {/* <Route exact path="/login" element={<Login />} />
-      <Route exact path="/register" element={<Register />} />
-      <Route exact path="/dashboard" element={<Dashboard />} />
-      <Route exact path="/donor" element={<DonorPage />} />
-      <Route exact path="/patient" element={<PatientPage />} />
-      <Route exact path="/resetpassword" element={<ResetPassword />} />
-      <Route exact path="/forgotpassword" element={<ForgotPassword />} /> */}
     </Routes>
   </>
 );
