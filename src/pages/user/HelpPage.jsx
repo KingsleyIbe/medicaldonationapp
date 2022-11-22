833cacc5966d6ff9940495e88c5408f47deaff9b
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-22 16:12:59 +0100

    Add page for help

---
+++ src/pages/user/HelpPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Help from '../../components/user/dashboard/help';
+
+const HelpPage = () => (
+  <div>
+    <Help />
+  </div>
+);
+
+export default HelpPage;
--- src/routerPage.js
+++ src/routerPage.js
@@ -5,6 +5,7 @@ import LandingPage from './pages/landingPage/landingPage';
 import DashboardPage from './pages/user/DashboardPage';
 import DonateItemPage from './pages/user/DonateItemPage';
 import FeedbackPage from './pages/user/FeedbackPage';
+import HelpPage from './pages/user/HelpPage';
 import KYCPage from './pages/user/KYCPage';
 import RecentActivitiesPage from './pages/user/RecentActivitiesPage';
 
@@ -18,7 +19,7 @@ const RouterPage = () => (
       <Route exact path="/dashboard/request-item" element={<DashboardPage />} />
       <Route exact path="/dashboard/message" element={<DashboardPage />} />
       <Route exact path="/dashboard/recent-activities" element={<RecentActivitiesPage />} />
-      <Route exact path="/dashboard/help" element={<DashboardPage />} />
+      <Route exact path="/dashboard/help" element={<HelpPage />} />
       <Route exact path="/dashboard/policy" element={<DashboardPage />} />
       <Route exact path="/dashboard/feedback" element={<FeedbackPage />} />
       <Route exact path="/faq" element={<FaqPage />} />
