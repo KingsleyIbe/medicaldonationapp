79f275f1f41efa14e3a76d57ad60f0247708abc7
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-22 18:13:50 +0100

    Update donate item table

--- src/components/user/dashboard/historySummary/HistorySummary.js
+++ src/components/user/dashboard/historySummary/HistorySummary.js
@@ -1,27 +1,31 @@
+import {
+  faDashboard,
+} from '@fortawesome/free-solid-svg-icons';
+
 const Data = [
   {
     id: 1,
     totalNumber: '0',
     title: 'Total Donation',
-    icon: 'faBell',
+    icon: faDashboard,
   },
   {
     id: 2,
     totalNumber: '0',
     title: 'Total Items Received',
-    icon: 'faBell',
+    icon: faDashboard,
   },
   {
     id: 3,
     totalNumber: '0',
     title: 'Number of Reviews',
-    icon: 'faBell',
+    icon: faDashboard,
   },
   {
     id: 4,
     totalNumber: '0',
     title: 'Pending Requests',
-    icon: 'faBell',
+    icon: faDashboard,
   },
 ];
 
