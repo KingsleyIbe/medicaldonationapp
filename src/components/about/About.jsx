24c53f75b73e74f8120b6a2947407cb80ffb4f0a
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-27 23:38:07 +0100

    Add mission and vision section on the about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,11 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBolt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBolt, faLinkedin,
+  faTwitter, faGithub, faInstagram, faFacebook,
+} from '@fortawesome/free-solid-svg-icons';
 import Doctors from '../../assets/images/doctors.jpg';
 
 const About = () => (
@@ -80,6 +85,84 @@ const About = () => (
             <p className="opacity-[0.6]">We work together to maximize efficiency and effectiveness for our donor community.</p>
           </li>
         </ul>
+      </div>
+      <div className="flex flex-row gap-10 mb-10">
+        <div className="bg-[#fff] border border-1-solid rounded-[8px] p-10 text-center">
+          <img src={Doctors} alt="Kingsley profile logo" className="max-w-[100px] h-[100px] rounded-[100%] m-auto" />
+          <h3 className="text-center text-[#A03] font-bold text-[25px] my-2">Kingsley Ibe</h3>
+          <p className="text-center opacity-[0.6] mb-2">Frontend Developer/Project Lead</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur quo deserunt voluptates eos odio repellat magnam doloremque,
+            incidunt neque blanditiis facilis laboriosam dolores debitis assumenda quisquam
+            tenetur officiis minus ipsam.
+          </p>
+          <div className="flex flex-row gap-5 items-center text-center text-[25px] text-[#17a2b8]">
+            <a href="https://www.linkedin.com/in/kingsley-ibe/" target="__blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a href="https://web.facebook.com/ibekingsley.chibueze/" target="__blank"><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href="https://twitter.com/ibekingsley2" target="__blank"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="https://twitter.com/ibekingsley2" target="__blank"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="https://github.com/KingsleyIbe" target="__blank"><FontAwesomeIcon icon={faGithub} /></a>
+          </div>
+        </div>
+        <div className="bg-[#fff] border border-1-solid rounded-[8px] p-10 text-center">
+          <img src={Doctors} alt="Kingsley profile logo" className="max-w-[100px] h-[100px] rounded-[100%] m-auto" />
+          <h3 className="text-center text-[#A03] font-bold text-[25px] my-2">Michael David</h3>
+          <p className="text-center opacity-[0.6] mb-2">Backend Developer</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur quo deserunt voluptates eos odio repellat magnam doloremque,
+            incidunt neque blanditiis facilis laboriosam dolores debitis assumenda quisquam
+            tenetur officiis minus ipsam.
+          </p>
+          <div className="flex flex-row gap-5 items-center text-center text-[25px] text-[#17a2b8]">
+            <a href="https://www.linkedin.com/in/michael-david-oyenuga" target="__blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a href="https://www.linkedin.com/in/michael-david-oyenuga" target="__blank"><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href="https://twitter.com/David_Gahd" target="__blank"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="https://twitter.com/David_Gahd" target="__blank"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="https://github.com/Gahdloot" target="__blank"><FontAwesomeIcon icon={faGithub} /></a>
+          </div>
+        </div>
+        <div className="bg-[#fff] border border-1-solid rounded-[8px] p-10 text-center">
+          <img src={Doctors} alt="Kingsley profile logo" className="max-w-[100px] h-[100px] rounded-[100%] m-auto" />
+          <h3 className="text-center text-[#A03] font-bold text-[25px] my-2">Josephine Bonka</h3>
+          <p className="text-center opacity-[0.6] mb-2">UI/UX Designer</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur quo deserunt voluptates eos odio repellat magnam doloremque,
+            incidunt neque blanditiis facilis laboriosam dolores debitis assumenda quisquam
+            tenetur officiis minus ipsam.
+          </p>
+          <div className="flex flex-row gap-5 items-center text-center text-[25px] text-[#17a2b8]">
+            <a href="https://www.linkedin.com/in/josephine-bonka-250263207" target="__blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a href="https://www.linkedin.com/in/josephine-bonka-250263207" target="__blank"><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href="https://twitter.com/josephineeng2" target="__blank"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="https://twitter.com/josephineeng2" target="__blank"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="https://github.com/josephineEng" target="__blank"><FontAwesomeIcon icon={faGithub} /></a>
+          </div>
+        </div>
+        <div className="bg-[#fff] border border-1-solid rounded-[8px] p-10 text-center">
+          <img src={Doctors} alt="Kingsley profile logo" className="max-w-[100px] h-[100px] rounded-[100%] m-auto" />
+          <h3 className="text-center text-[#A03] font-bold text-[25px] my-2">Adio Usman</h3>
+          <p className="text-center opacity-[0.6] mb-2">Market Researcher</p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur quo deserunt voluptates eos odio repellat magnam doloremque,
+            incidunt neque blanditiis facilis laboriosam dolores debitis assumenda quisquam
+            tenetur officiis minus ipsam.
+          </p>
+          <div className="flex flex-row gap-5 items-center text-center text-[25px] text-[#17a2b8]">
+            <a href="https://www.linkedin.com/in/adiousman/" target="__blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a href="https://www.linkedin.com/in/adiousman/" target="__blank"><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href="https://twitter.com/usafod" target="__blank"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="https://twitter.com/usafod" target="__blank"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="https://github.com/usafod" target="__blank"><FontAwesomeIcon icon={faGithub} /></a>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row justify-between contact-border p-10 my-5 text-[#A03]">
+        <h3 className="text-[25px]">We are with you in every step of your donation</h3>
+        <Link to="/contact" className="px-[21px] py-[12px] rounded-[6px] text-[#A03] bg-[#17a2b8]"><button type="button">Conact Us</button></Link>
       </div>
     </div>
   </div>
