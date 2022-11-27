81f0724eb1f238f6ddda4d39e8a2cf63a9f8ffe2
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-27 20:49:58 +0100

    Add style to the contact page

--- src/components/contactUs/ContactUs.jsx
+++ src/components/contactUs/ContactUs.jsx
@@ -6,39 +6,39 @@ import group1 from '../../assets/images/group1.png';
 import group from '../../assets/images/group.png';
 
 const ContactUs = () => (
-  <section className="px-10 w-[90%] m-auto">
+  <section className="px-10 w-[100%]">
     <div><h1 className="font-bold text-[35px] mb-[50px] text-center">Contact Us</h1></div>
-    <div className="lg:flex gap-10 items-center">
+    <div className="lg:flex gap-10">
       <div>
         <form action="https://formspree.io/f/mgeqzkkb" method="POST" className="mb-10">
-          <div className="lg:flex md:flex sm:flex grid gap-5">
-            <label htmlFor="firstName" className="mt-10 text-[22px] text-[#303A42] grid">
+          <div className="lg:flex md:flex sm:flex flex-row gap-5">
+            <label htmlFor="firstName" className="mt-10 text-[#303A42] grid">
               First name
-              <input type="text" name="firstName" required placeholder="First name" className="grid my-2 border-none contact-input p-2 rounded-[8px]" />
+              <input type="text" name="firstName" required placeholder="First name" className="my-2 border-none contact-input p-2 rounded-[8px]" />
             </label>
-            <label htmlFor="secondName" className="grid mt-10 text-[22px] text-[#303A42]">
+            <label htmlFor="secondName" className="grid mt-10 text-[#303A42]">
               Second name
               <input type="text" name="secondName" required placeholder="Second name" className="my-2 border-none contact-input p-2 rounded-[8px]" />
             </label>
           </div>
           <div className="">
-            <label htmlFor="email" className="grid mt-10 text-[22px] text-[#303A42]">
+            <label htmlFor="email" className="grid mt-10 text-[#303A42]">
               Email
               <input type="email" name="email" required placeholder="Ex example@email.com" className="my-2 border-none contact-input p-2 rounded-[8px]" />
             </label>
-            <label htmlFor="message" className="grid mt-10 text-[22px] text-[#303A42]">
+            <label htmlFor="message" className="grid mt-10 text-[#303A42]">
               What can we help you with?
               <textarea type="text" name="message" required placeholder="Type your message here" className="my-2 border-none contact-input p-2 rounded-[8px]" rows="8" cols="50" maxLength="200" />
             </label>
           </div>
-          <button type="submit" className="rounded-[8px] bg-[#E65C00] mt-5 px-[21px] py-[12px] w-[264px] text-[#fff]">Send</button>
+          <button type="submit" className="rounded-[8px] w-[150px] bg-[#17a2b8] mt-5 px-[21px] py-[12px] text-[#fff]">Send</button>
         </form>
       </div>
       <div className="md:mb-[145px] md:mt-[100px] sm:mb-[145px] sm:mt-[100px]">
         <div className="text-center">
           <p>
             We would be happy to hear from you,
-            Please fill in the form below or mail us your requirements to
+            Please fill in the form below or mail us your enquiries to
           </p>
           <Link
             to="#"
@@ -46,15 +46,15 @@ const ContactUs = () => (
               window.location.href = 'mailto:info@prudenceconcept.com';
               e.preventDefault();
             }}
-            className="underline mt-2 text-[#E65C00]"
+            className="underline mt-2 text-[#A03]"
           >
             info@lifeforce.com
           </Link>
         </div>
-        <div className="mt-[100px] lg:mt-0 md:mt-0 relative lg:left-10 lg:bottom-[-145px] md:left-10 md:bottom-[-145px] left-0 bottom-0">
+        <div className="mt-[100px] lg:mt-0 md:mt-0 relative lg:left-0 lg:bottom-[-145px] md:left-10 md:bottom-[-145px] left-0 bottom-0">
           <img src={call} alt="Call design" />
-          <img src={group1} alt="Group design" className="absolute lg:top-[-250px] lg:left-[170px] md:top-[-250px] md:left-[250px] top-[-160px] left-[50px]" />
-          <img src={group} alt="Group design" className="absolute lg:top-[-200px] lg:left-[-120px] md:top-[-250px] md:left-[-180px] top-[-100px] left-[-50px]" />
+          <img src={group1} alt="Group design" className="absolute lg:top-[-250px] lg:left-[260px] md:top-[-250px] md:left-[250px] top-[-160px] left-[50px]" />
+          <img src={group} alt="Group design" className="absolute lg:top-[-240px] lg:left-[-120px] md:top-[-250px] md:left-[-180px] top-[-100px] left-[-50px]" />
         </div>
       </div>
     </div>
