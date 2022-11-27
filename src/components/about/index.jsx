3460a49fe405a0c2e8ee18c5f2c6bb9170473f37
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-11-27 20:04:13 +0100

    Add about page

---
+++ src/components/about/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../common/Header';
+import About from './About';
+
+const index = () => (
+  <div>
+    <Header />
+    <About />
+  </div>
+);
+
+export default index;
---
+++ src/pages/about/AboutPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AboutUs from '../../components/about';
+
+const AboutPage = () => (
+  <div>
+    <AboutUs />
+  </div>
+);
+
+export default AboutPage;
