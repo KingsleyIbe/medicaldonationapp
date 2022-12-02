7369c8902eab244a65b7bdfda34fe51eca17b2bc
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-12-02 08:17:37 +0100

    Resolve git commit conflict

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
-import App from './App';
-
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const uri = 'https://life-force.vercel.app/kyc';
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        redirectUri={uri}
-      >
-        <App />
-      </Auth0Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:0511a3ec2d1630862b41bcba9efa12e414f143e9616e2513c94b0172ad0e5699
+size 382
