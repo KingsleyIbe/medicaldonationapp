75d1b6874e2a3057dfec3a0b5b071245997189d4
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-12-02 07:35:04 +0100

    Fix git commit conflicts

--- src/components/user/dashboard/policy/PolicyDocs.jsx
+++ src/components/user/dashboard/policy/PolicyDocs.jsx
@@ -177,12 +177,12 @@ const PolicyDocs = () => (
       identifiable information may include, but is not limited to:
     </p>
     <ul className="ml-10">
-      <li>Email address</li>
-      <li>First name and last name</li>
-      <li>Phone number</li>
-      <li>Address, State, Province, ZIP/Postal code, City</li>
-      <li>National Identity Number (NIN)</li>
-      <li>Usage Data</li>
+      <li> ● Email address</li>
+      <li> ● First name and last name</li>
+      <li> ● Phone number</li>
+      <li> ● Address, State, Province, ZIP/Postal code, City</li>
+      <li> ● National Identity Number (NIN)</li>
+      <li> ● Usage Data</li>
     </ul>
     <h3 className="text-[#17a2b8] text-[18px] mb-2">Usage Data</h3>
     <p>Usage Data has collected automatically when using the Service.</p>
@@ -212,12 +212,12 @@ const PolicyDocs = () => (
       log in to use the Service through the
       following Third-party Social Media Services:
     </p>
-    <ol className="my-2 ml-10">
-      <li>Google</li>
-      <li>Facebook</li>
-      <li>Twitter</li>
-      <li> LinkedIn</li>
-    </ol>
+    <ul className="my-2 ml-10">
+      <li> ●  Google</li>
+      <li> ●  Facebook</li>
+      <li> ●  Twitter</li>
+      <li> ●  LinkedIn</li>
+    </ul>
     <p>
       Suppose You decide to register through or otherwise grant us access to a
       Third-Party Social Media Service. In that case, We may collect Personal data
@@ -304,12 +304,260 @@ const PolicyDocs = () => (
       please visit our Cookies Policy or the Cookies section of our Privacy Policy.
     </p>
     <h2 className="text-[#17a2b8] text-[25px] mb-2">Use of Your Personal Data</h2>
-    <p>The Company may use Personal Data for the following purposes:</p>
-    <ul>
+    <p className="mb-2">The Company may use Personal Data for the following purposes:</p>
+    <ul className="mb-2 ml-10">
       <li>
         <b>To provide and maintain our Service, </b>
         including monitoring the usage of our Service.
       </li>
+      <li>
+        <b>To manage Your Account: </b>
+        to manage Your registration as a user of the Service.
+        The Personal Data You provide can give You access to different
+        functionalities of the Service that are available
+        to You as a registered user.
+
+      </li>
+      <li>
+        <b>For the performance of a contract: </b>
+        the development, compliance and undertaking of
+        the purchase contract for the products,
+        items or services You have purchased or of any
+        other contract with Us through the Service.
+      </li>
+      <li>
+        <b>To contact You: </b>
+        To contact You by email, telephone calls, SMS, or other
+        equivalent forms of electronic communication, such as a mobile application&#39;s
+        push notifications regarding updates or informative communications related
+        to the functionalities, products or contracted services,
+        including the security updates, when necessary or reasonable
+        for their implementation.
+      </li>
+      <li>
+        <b>To provide You </b>
+        with news, special offers and general information about other goods,
+        services and events which we offer that are similar to those that you
+        have already purchased or enquired about unless You have
+        opted not to receive such information.
+      </li>
+      <li>
+        <b>To manage Your requests: </b>
+        To attend to and manage Your requests to Us.
+      </li>
+      <li>
+        <b>For business transfers: </b>
+        We may use Your information to evaluate or conduct a merger,
+        divestiture, restructuring, reorganization, dissolution,
+        or another sale or transfer of some or all of Our assets,
+        whether as a going concern or as part of bankruptcy, liquidation,
+        or similar proceeding, in which Personal Data held by Us about
+        our Service users is among the assets transferred.
+      </li>
+      <li>
+        <b>For other purposes: </b>
+        We may use Your information for other purposes,
+        such as data analysis, identifying usage trends,
+        determining the effectiveness of our promotional
+        campaigns and evaluating and improving our Service,
+        products, services, marketing and your experience.
+      </li>
+    </ul>
+    <p className="mb-2">We may share Your personal information in the following situations:</p>
+    <ul className="mb-2 ml-10">
+      <li>
+        <b> With Service Providers: </b>
+        We may share Your personal information with Service Providers
+        to monitor and analyze the use of our Service, and to contact You.
+      </li>
+      <li>
+        <b>For business transfers: </b>
+        We may share or transfer Your personal information in connection with,
+        or during negotiations of, any merger, sale of Company assets, financing,
+        or acquisition of all or a portion of Our business to another company.
+      </li>
+      <li>
+        <b>For the performance of a contract: </b>
+        the development, compliance and undertaking of
+        the purchase contract for the products,
+        items or services You have purchased or of any
+        other contract with Us through the Service.
+      </li>
+      <li>
+        <b>With Affiliates: </b>
+        We may share Your information with Our affiliates,
+        in which case we will require those affiliates to honour
+        this Privacy Policy. Affiliates include Our parent company
+        and any other subsidiaries, joint venture partners or other
+        companies that We control or that are under common control with Us.
+      </li>
+      <li>
+        <b> With business partners: </b>
+        We may share Your information with Our business partners
+        to offer You certain products, services or promotions.
+
+      </li>
+      <li>
+        <b>With other users: </b>
+        when You share personal information or otherwise interact
+        in public areas with other users, such information may be
+        viewed by all users and may be publicly distributed outside.
+        If You interact with other users or register through a
+        Third-Party Social Media Service, Your contacts on the
+        Third-Party Social Media Service may see Your name,
+        profile, pictures and description of Your activity.
+        Similarly, other users will be able to view descriptions of
+        Your activity, communicate with You and view Your profile.
+      </li>
+      <li>
+        <b>With Your consent: </b>
+        We may disclose Your personal information for any other purpose with Your consent.
+      </li>
+    </ul>
+    <h3 className="text-[#17a2b8] text-[18px] mb-2">Retention of Your Personal Data</h3>
+    <p className="mb-2">
+      The Company will retain Your Personal Data only for as long as is necessary for the
+      purposes set out in this Privacy Policy. We will retain and use Your Personal
+      Data to the extent necessary to comply with our legal obligations
+      (for example, if we are required to retain your data to comply with applicable laws),
+      resolve disputes, and enforce our legal agreements and policies.
+    </p>
+    <p>
+      The Company will also retain Usage Data for internal analysis purposes.
+      Usage Data is generally retained for a shorter period of time,
+      except when this data is used to strengthen the security or to improve
+      the functionality of Our Service, or We are legally obligated to
+      retain this data for longer time periods.
+    </p>
+    <h3 className="text-[#17a2b8] text-[18px] my-2">Transfer of Your Personal Data</h3>
+    <p>
+      Your information, including Personal Data, is processed at the Company&#39;s
+      operating offices and in any other places where the parties
+      involved in the processing are located. It means that this
+      information may be transferred to — and maintained on — computers
+      located outside of Your state, province, country or other governmental
+      jurisdiction where the data protection laws
+      may differ from those from Your jurisdiction.
+    </p>
+    <p className="my-2">
+      Your consent to this Privacy Policy followed by
+      Your submission of such information represents
+      Your agreement to that transfer.
+    </p>
+    <p>
+      The Company will take all steps reasonably necessary
+      to ensure that Your data is treated securely and in
+      accordance with this Privacy Policy and no transfer of
+      Your Personal Data will take place to an organization or a
+      country unless there are adequate controls in place including
+      the security of Your data and other personal information.
+    </p>
+    <h3 className="text-[#17a2b8] text-[18px] my-2">Delete Your Personal Data</h3>
+    <p>
+      You have the right to delete or request that We assist
+      in deleting the Personal Data that We have collected about You.
+    </p>
+    <p className="my-2">
+      Our Service may give You the ability to delete
+      certain information about You from within the Service.
+    </p>
+    <p>
+      You may update, amend, or delete Your information at any
+      time by signing in to Your Account, if you have one, and
+      visiting the account settings section that allows you to manage
+      Your personal information. You may also contact Us to request access to,
+      correct, or delete any personal information that You have provided to Us.
+    </p>
+    <p className="my-2">
+      Please note, however, that We may need to retain certain
+      information when we have a legal obligation or lawful basis to do so.
+    </p>
+    <h3 className="text-[#17a2b8] text-[20px] mb-2">Disclosure of Your Personal Data</h3>
+    <h4 className="text-[#17a2b8] text-[18px] mb-2">Business Transactions</h4>
+    <p className="mb-2">
+      If the Company is involved in a merger, acquisition or asset sale,
+      Your Personal Data may be transferred. We will provide notice before
+      Your Personal Data is transferred and becomes
+      subject to a different Privacy Policy.
+    </p>
+    <h4 className="text-[#17a2b8] text-[18px] mb-2">Law enforcement</h4>
+    <p className="mb-2">
+      Under certain circumstances, the Company may be
+      required to disclose Your Personal Data if required
+      to do so by law or in response to valid requests by
+      public authorities (e.g. a court or a government agency).
+
+    </p>
+    <h4 className="text-[#17a2b8] text-[18px] mb-2">Other legal requirements</h4>
+    <p className="mb-2">
+      The Company may disclose Your Personal Data in
+      the good faith belief that such action is necessary to:
+    </p>
+    <ul className="ml-10 mb-2">
+      <li> ● Comply with a legal obligation</li>
+      <li> ● Protect and defend the rights or property of the Company</li>
+      <li> ● Prevent or investigate possible wrongdoing in connection with the Service</li>
+      <li> ● Protect the personal safety of Users of the Service or the public</li>
+      <li> ● Protect against legal liability</li>
+    </ul>
+    <h4 className="text-[#17a2b8] text-[18px] mb-2">Security of Your Personal Data</h4>
+    <p className="mb-2">
+      The security of Your Personal Data is important to Us,
+      but remember that no method of transmission over the Internet,
+      or method of electronic storage is 100% secure. While We strive
+      to use commercially acceptable means to protect Your Personal Data,
+      We cannot guarantee its absolute security.
+    </p>
+    <h2 className="text-[#17a2b8] text-[25px] my-5">Children&#39;s Privacy</h2>
+    <p className="mb-2">
+      Our Service does not address anyone under the age of 13.
+      We do not knowingly collect personally identifiable information
+      from anyone under the age of 13. If You are a parent or guardian
+      and You are aware that Your child has provided Us with Personal Data,
+      please contact Us. If We become aware that We have collected Personal
+      Data from anyone under the age of 13 without verification of parental consent,
+      We take steps to remove that information from Our servers.
+    </p>
+    <p>
+      If We need to rely on consent as a legal basis
+      for processing Your information and Your country
+      requires consent from a parent, We may require Your parent&#39;s
+      consent before We collect and use that information.
+    </p>
+    <h2 className="text-[#17a2b8] text-[25px] my-5">Links to Other Websites</h2>
+    <p className="mb-2">
+      Our Service may contain links to other websites that are not
+      operated by Us. If You click on a third-party link, You will
+      be directed to that third-party&#39;s site. We strongly advise You
+      to review the Privacy Policy of every site You visit.
+    </p>
+    <p>
+      We have no control over and assume no responsibility
+      for the content, privacy policies or practices
+      of any third-party sites or services.
+    </p>
+    <h2 className="text-[#17a2b8] text-[25px] my-5">Changes to this Privacy Policy</h2>
+    <p className="mb-2">
+      We may update Our Privacy Policy from time to time.
+      We will notify You of any changes by posting the
+      new Privacy Policy on this page.
+    </p>
+    <p>
+      We will let You know via email and/or a prominent
+      notice on Our Service, prior to the change becoming
+      effective and update the &#34;Last updated&#34; date at the top of this Privacy Policy.
+    </p>
+    <p className="my-2">
+      You are advised to review this Privacy Policy periodically for any changes.
+      Changes to this Privacy Policy are effective when they are posted on this page.
+    </p>
+    <h2 className="text-[#17a2b8] text-[25px] my-5">Contact Us</h2>
+    <p className="mb-2">
+      If you have any questions about this Privacy Policy, You can contact us:
+    </p>
+    <ul className="ml-10 mb-10">
+      <li>By email: support@lifeforce.com</li>
+      <li>By visiting this page on our website: http://www.lifeforce.com/contact</li>
     </ul>
   </div>
 );
